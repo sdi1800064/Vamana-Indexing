7448720aa1419eb650f3d42d168882edaeaf024f
Thomas Karagiannis <sdi1800064@uoa.gr>
2025-01-12 14:07:49 +0200

    results

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 commands=()
-for t in {5..1}; do
+for t in {7..6}; do
     commands+=("./stitchedVamana -b testSets/dummy-data.bin -R 60 -a 1.2 -L 120 -t $t")
     commands+=("./recallSticthedVamana -b testSets/dummy-data.bin -q testSets/dummy-queries.bin -g testSets/groundtruth.ivecs -R 60 -a 1.2 -L 120 -k 100 -t $t --graph graphs/stitchedGraph")
     commands+=("./stitchedVamana -b testSets/dummy-data.bin -R 20 -a 1.2 -L 200 -t $t")
     commands+=("./recallSticthedVamana -b testSets/dummy-data.bin -q testSets/dummy-queries.bin -g testSets/groundtruth.ivecs -R 20 -a 1.2 -L 200 -k 100 -t $t --graph graphs/stitchedGraph")
 done
 
-for t in {5..1}; do
+for t in {7..6}; do
     commands+=("./stitchedVamana -b testSets/contest-data-release-1m.bin -R 60 -a 1.2 -L 120 -t $t")
     commands+=("./recallSticthedVamana -b testSets/contest-data-release-1m.bin -q testSets/contest-queries-release-1m.bin -g testSets/neighbors1m.ivecs -R 60 -a 1.2 -L 120 -k 100 -t $t --graph graphs/stitchedGraph")
     commands+=("./stitchedVamana -b testSets/contest-data-release-1m.bin -R 20 -a 1.2 -L 200 -t $t")
